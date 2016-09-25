c9b47252ec8d7485a823a4ca8b6f46031fd4e3d8
Nicooo <saoutn@gmail.com>
2016-09-25 17:59:47 +0200

    suppr fichiers js

--- angular2-tour-of-heroes/app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Initialise la plateforme de l'application
-// Utilise la plateforme pour amorçer AppModule
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
