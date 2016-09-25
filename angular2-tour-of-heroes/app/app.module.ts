36ee65d882ce0d712a88acf4ac0b1cc0e6e5be44
Nicooo <saoutn@gmail.com>
2016-09-25 17:56:16 +0200

    Déplacement des fichiers Tour of Heroes

--- angular2-tour-of-heroes/app/app.module.ts
+++ angular2-tour-of-heroes/app/app.module.ts
@@ -4,11 +4,22 @@ import { NgModule }         from '@angular/core';
 // import de BrowserModule car application web
 import { BrowserModule }    from '@angular/platform-browser';
 
+import { FormsModule }      from '@angular/forms';
+
 import { AppComponent }     from './app.component';
 
+import { HeroDetailComponent }  from './hero-detail.component';
+
 @NgModule({
-    imports:        [ BrowserModule ],
-    declarations:   [ AppComponent ],
+    // contient la liste des modules externes utilisés par l'application
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
     bootstrap:      [ AppComponent ]
 })
 
---
+++ angular2-tour-of-heroes/app/hero.js
@@ -0,0 +1,9 @@
+"use strict";
+// Classe Hero
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
